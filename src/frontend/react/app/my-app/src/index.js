cbd6ad3d7aebafe380d16d9a2cf7e7346409881b
ryusa <kommsussertad@gmail.com>
2020-07-29 22:05:30 +0900

    feature(react): node_modulesを除外してコンテナを立ち上げる基盤部分を作成

--- src/frontend/react/app/my-app/src/index.js
+++ src/frontend/react/app/my-app/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
